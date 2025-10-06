53cf86bcbfd4672c51041cc1b1de7d92cf92d0cf
duckbill70 <andrew.platts@hotmail.com>
2025-10-06 17:17:17 +0100

    Cleaner with logging

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 # Install Node, CocoaPods, and yarn using Homebrew.
-brew install node
+echo "===== Installling CocoaPods ====="
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+echo "===== Installing Node.js ====="
+brew install node
+echo "===== Installing yarn ====="
 brew install yarn
 
 # Install dependencies
-yarn
+echo "===== Running yarn install ====="
+yarn install
+echo "===== Running pod install ====="
+cd ios
 pod install
